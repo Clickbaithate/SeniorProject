adad3b89b93862ab0bf7395dc0cd75accfcdf694
Clickbaithate <gg.guzzman1514@gmail.com>
2024-09-09 20:37:13 -0500

    Adding bare minimum vite project using react and javascript

---
+++ client/cine-vault/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+// In terminal, cd to client folder then cine-vault folder and run "npm i" then run "npm run dev" to start frontend
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App;
