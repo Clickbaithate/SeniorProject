f24f773a81d4f4da467314ba3b70fea9a66951d2
Clickbaithate <gg.guzzman1514@gmail.com>
2024-09-09 20:46:51 -0500

    Added routes and getting user session

--- client/cine-vault/src/App.jsx
+++ client/cine-vault/src/App.jsx
@@ -1,13 +1,61 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react';
+import supabase from './config/supabaseClient';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import SignUpPage from './pages/SignUpPage';
+import LandingPage from './pages/LandingPage';
+import HomePage from './pages/homePage';
+import ProfileSetup from './pages/profileSetup';
+import EmailConfirmationPage from './pages/EmailConfirmationPage';
+import LoginPage from './pages/LoginPage';
 
-// In terminal, cd to client folder then cine-vault folder and run "npm i" then run "npm run dev" to start frontend
 function App() {
+
+  const [session, setSession] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // When the website loads, we check if the user is logged in or not
+  useEffect(() => {
+
+    // Getting user's session through supabase, basically a cookie but not really
+    const fetchSession = async () => {
+      const { data: { session } } = await supabase.auth.getSession();
+      setSession(session);
+      setLoading(false);
+    };
+
+    fetchSession();
+
+    // Listening for any change in a user's session, signed in, signed out, etc
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => {
+      subscription?.unsubscribe();
+    };
+  }, []);
+
+  // While we get user's session state...
+  if (loading) return <div>Loading...</div>;
+
+  // Establishing routes
+  // If a user is not logged in, redirect them accordingly
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Router>
+      <Routes> 
+        <Route path="/" element={session ? <Navigate to="/homePage" /> : <LandingPage />} />
+
+        <Route path="/signUp" element={session ? <Navigate to="/homePage" /> : <SignUpPage /> } />
+        <Route path="/login" element={session ? <Navigate to="/homePage" /> : <LoginPage /> } />
+        
+        <Route path="/homePage" element={session ? <HomePage /> : <Navigate to="/" />} />
+        <Route path="/profileSetup" element={session ? <ProfileSetup /> : <Navigate to="/" />} />
+        <Route path="/emailConfirmationPage" element={<EmailConfirmationPage />} />
+
+        <Route path="*" element={session ? <Navigate to="/homePage" /> : <Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
