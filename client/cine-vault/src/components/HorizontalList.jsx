a0d0fc268c376641f168af672a97fe345a426ab3
Clickbaithate <gael.guzman01@utrgv.edu>
2024-10-03 11:25:43 -0500

    Minor Fixes

--- client/cine-vault/src/components/HorizontalList.jsx
+++ client/cine-vault/src/components/HorizontalList.jsx
@@ -33,7 +33,7 @@ const HorizontalList = ({ movies, genres, theme = 'light' }) => {
     <div className="relative mx-12 pt-4">
 
       {/* Left Button */}
-      <button className={`absolute left-0 top-1/2 transform ${movies ? "-translate-y-1/2" : "-translate-y-1/3"} rounded-full p-2`} onClick={scrollLeft} >
+      <button className={`absolute left-0 top-1/2 transform ${movies ? "-translate-y-1/2" : "-translate-y-1/3"} rounded-full p-2 ml-4 z-10`} onClick={scrollLeft} >
         <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 hover:bg-white/70">
           <svg className="w-4 h-4 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
@@ -42,7 +42,7 @@ const HorizontalList = ({ movies, genres, theme = 'light' }) => {
       </button>
 
       {/* List */}
-      <div ref={scrollRef} className={`flex space-x-4 overflow-x-auto scrollbar-none py-4`} >
+      <div ref={scrollRef} className={`flex space-x-4 overflow-x-auto scrollbar-none py-4 px-4`} >
         {/* Movie */}
         {
           movies ? (
@@ -51,7 +51,7 @@ const HorizontalList = ({ movies, genres, theme = 'light' }) => {
             ))
           ) : (
             genres.map((genre, index) => (
-              <GenreCard genre={genre} theme={theme} index={index} />
+              <GenreCard genre={genre} theme={theme} index={index} key={index} />
             ))
           )
         }
@@ -60,7 +60,7 @@ const HorizontalList = ({ movies, genres, theme = 'light' }) => {
       </div>
 
       {/* Right Button */}
-      <button className={`absolute right-0 top-1/2 transform ${movies ? "-translate-y-1/2" : "-translate-y-1/3"} rounded-full p-2`} onClick={scrollRight} >
+      <button className={`absolute right-0 top-1/2 transform ${movies ? "-translate-y-1/2" : "-translate-y-1/3 mr-4"} rounded-full p-2 `} onClick={scrollRight} >
         <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 hover:bg-white/70">
           <svg className="w-4 h-4 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
--- client/cine-vault/src/components/movieCard.jsx
+++ client/cine-vault/src/components/movieCard.jsx
@@ -7,7 +7,7 @@ const MovieCard = ({movie, theme, index}) => {
   }
 
   return (
-    <div key={index} onClick={() => handleOnClick(movie.title)} className="max-w-[175px] bg-transparent flex-shrink-0 cursor-pointer ">
+    <div key={index} onClick={() => handleOnClick(movie.title)} className="transition-all ease-in-out duration-500 transform hover:scale-110 min-w-[175px] max-w-[175px] bg-transparent flex-shrink-0 cursor-pointer ">
       <img src={movie.poster} alt={movie.title} className="w-full h-64 object-cover rounded-3xl mb-2" />
       <h2 className={`text-md truncate mx-2 font-body font-semibold ${theme === "light" ? "text-black" : "text-white"}`}> {movie.title} </h2>
     </div>
