a203cce3a6ac80af4d79165ad2e325e27c61e5e8
Clickbaithate <gg.guzzman1514@gmail.com>
2024-11-17 13:56:56 -0600

    test

--- client/cine-vault/src/pages/moviePage.jsx
+++ client/cine-vault/src/pages/moviePage.jsx
@@ -83,7 +83,7 @@ const MoviePage = () => {
       if (!movie) return; // If movie data is not available, do not call the API
 
       try {
-        const data = { data: id }
+        const data = { data: [id] }
         const headers = { 'Content-Type': 'application/json' };
         const response = await axios.post(
           `https://gaelguzman.us-east-1.aws.modelbit.com/v1/get_recommendations_chunked/latest`,
